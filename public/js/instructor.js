8cdea6e50cd5ca8a4ecbcf8717e44e85bb27048a
Diego Baeza <diego.icinf@gmail.com>
2024-06-05 19:33:00 -0400

    Correción de instructores

--- public/js/instructor.js
+++ public/js/instructor.js
@@ -1,9 +1,17 @@
 jQuery(document).ready( function(){
 
     var height_blue_box = jQuery('#box-white-instructor-description').height();
-    var dif_space = height_blue_box - 465;
-    var total = dif_space < 0 ? dif_space * -1 : dif_space;
 
-    jQuery("#courses-instructor-box").css("margin-bottom" , Math.ceil(total));
+    if(Math.ceil(height_blue_box) >= 230){
+
+        console.log(Math.ceil(height_blue_box));
+
+        var dif_space = height_blue_box - 465;
+        var total = dif_space < 0 ? dif_space * -1 : dif_space;
+        jQuery("#courses-instructor-box").css("margin-bottom" , Math.ceil(total));
+
+    }else{
+        jQuery("#courses-instructor-box").css("margin-bottom" , 50);
+    }
 
 })
